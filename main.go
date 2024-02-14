88381700b66d6bf06a90bb58ad2e27fb47bf1d93
nquidox <nquidox@gmail.com>
2024-02-14 17:31:12 +0300

    random temp generation

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+)
+
+func rootHandler(rw http.ResponseWriter, r *http.Request) {
+	log.Println("Hello!")
+}
+
+func tempHandler(rw http.ResponseWriter, r *http.Request) {
+	temp := rand.Intn(30)
+	fmt.Fprintf(rw, "Random temp goes here: %d \n", temp)
+}
 
 func main() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/temp", tempHandler)
+
 	fmt.Println("Simple HTTP server")
+	log.Fatal(http.ListenAndServe("127.0.0.1:9001", nil))
+
 }
